7faace8aefb6714288f29dd227c1adf6fd0679de
dupree719 <isaiahdup7@gmail.com>
2022-11-08 15:56:55 -0500

    Added JSONIgnore to not display IDs

--- src/main/java/pizzaparlor/pizza/models/Customer.java
+++ src/main/java/pizzaparlor/pizza/models/Customer.java
@@ -1,5 +1,7 @@
 package pizzaparlor.pizza.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +12,7 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
     private String name;
--- src/main/java/pizzaparlor/pizza/models/Pizza.java
+++ src/main/java/pizzaparlor/pizza/models/Pizza.java
@@ -1,5 +1,7 @@
 package pizzaparlor.pizza.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
@@ -8,6 +10,7 @@ public class Pizza {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
     private String crust;
